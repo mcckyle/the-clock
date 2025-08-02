f3ea32db355df19e1fce965177a3d283b97f5170
Kyle McColgan <kdmbus21@gmail.com>
2025-08-01 22:58:26 -0500

    Improve some design choices.

--- src/components/TickMarks/TickMarks.jsx
+++ src/components/TickMarks/TickMarks.jsx
@@ -11,16 +11,27 @@ export default function TickMarks()
         <g className={styles.tickRing}>
             {Array.from({ length: 60 }).map((_, i) => {
                 const angle = (i * 6) * (Math.PI / 180);
-                const x1 = 100 + 85 * Math.cos(angle);
-                const y1 = 100 + 85 * Math.sin(angle);
-                const x2 = 100 + 95 * Math.cos(angle);
-                const y2 = 100 + 95 * Math.sin(angle);
+                const radiusInner = (i % 5 === 0) ? 82 : 87;
+                const radiusOuter = 95;
+
+                const x1 = 100 + radiusInner * Math.cos(angle);
+                const y1 = 100 + radiusInner * Math.sin(angle);
+                const x2 = 100 + radiusOuter * Math.cos(angle);
+                const y2 = 100 + radiusOuter * Math.sin(angle);
 
                 return (
-                    <line key = {i}
-                    x1={x1} y1={y1} x2={x2} y2={y2}
-                    data-second={i}
-                    className={`${styles.tick} ${((i + 15) % 5 === 0 ? styles.hourTick : '')}`}/>
+                    <line
+                        key = {i}
+                        x1={x1}
+                        y1={y1}
+                        x2={x2}
+                        y2={y2}
+                        data-second={i}
+                        className={`${styles.tick} ${i % 5 === 0 ? styles.hourTick : ''} ${
+                            i % 15 === 0 ? styles.quarterTick : ''
+                        }`}
+                         //${((i + 15) % 5 === 0 ? styles.hourTick : '')}`}
+                    />
                 );
             })}
         </g>
